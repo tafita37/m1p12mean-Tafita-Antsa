e96d30193610c993efbbbec6f7375b2b61018ec2
tafita37 <118960434+tafita37@users.noreply.github.com>
2025-04-08 20:14:38 +0300

    fix(dependance) : Réglage des répétitions des dépendances

--- src/app.config.ts
+++ src/app.config.ts
@@ -10,16 +10,16 @@ import localeFr from '@angular/common/locales/fr';
 
 registerLocaleData(localeFr);
 
-// ⚠️ Ajoutez ces imports (manquants dans votre config actuelle)
-import { registerLocaleData } from '@angular/common';
-import localeFr from '@angular/common/locales/fr';
-
 // Enregistre les règles de formatage françaises
 registerLocaleData(localeFr);
 
 export const appConfig: ApplicationConfig = {
     providers: [
-        provideRouter(appRoutes, withInMemoryScrolling({ anchorScrolling: 'enabled', scrollPositionRestoration: 'enabled' }), withEnabledBlockingInitialNavigation()),
+        provideRouter(
+            appRoutes, 
+            withInMemoryScrolling({ anchorScrolling: 'enabled', scrollPositionRestoration: 'enabled' }), 
+            withEnabledBlockingInitialNavigation()
+        ),
         provideHttpClient(withFetch()),
         provideAnimationsAsync(),
         providePrimeNG({ theme: { preset: Aura, options: { darkModeSelector: '.app-dark' } } }),
