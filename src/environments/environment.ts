1f92df9a33000efe0703fe3550e89316336514a8
tafita37 <118960434+tafita37@users.noreply.github.com>
2025-04-16 21:42:02 +0300

    add(dialog_new_mouvement) : Ajout d'une insertion de mouvement dans la liste de mouvement

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,5 +1,6 @@
 export const environment = {
   production: false,
+    // baseUrl: 'https://m1p12mean-tafita-antsa-api.vercel.app',
     baseUrl: 'http://localhost:5000',
     tokenClientStorage: "client_token",
     tokenManagerStorage : "manager_token",
